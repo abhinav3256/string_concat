dc5f7f680d95ff9656f0868f4e8d8cd5b6c8f181
abhinav3256 <abhinavkumar3256@gmail.com>
2022-06-14 16:50:24 +0530

    adding file

--- string_concat.go
+++ string_concat.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var word1 = []string{"ab", "c"}
+	var word1 = []string{"ab", "cd"}
 	var word2 = []string{"a", "bc"}
+	var a string
+	var b string
 
-	var a = strings.Join(word1[:], "")
-	var b = strings.Join(word2[:], "")
+	for i := 0; i < len(word1); i++ {
+		a = word1[i] + a
+	}
+	for i := 0; i < len(word2); i++ {
+		b = word2[i] + b
+	}
 
 	fmt.Println(a == b)
 }
